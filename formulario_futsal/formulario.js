cff02ee2be082b9e848249217f932a7eb459e049
Marcela dos Santos Capella <20231PF.INF_I0006@passofundo.ifsul.local>
2025-06-05 15:45:28 -0300

    atualizando

--- formulario_futsal/formulario.js
+++ formulario_futsal/formulario.js
@@ -11,35 +11,40 @@ class TimeFutsal{
     }
 }
 
-// 2. Seleciona o formulário e adiciona o evento de "submit"
-const formTime = document.getElementById("form-time");
+formTime.addEventListener("subimit", function(event){
+    event.preventDefault();
 
-// 3. Captura de dados usando FormData
-const dadosForm = new FormData(formTime);
-const nomeTime = dadosForm.get("nomeTime");
-const j1 = dadosForm.get("jogador1");
-const j2 = dadosForm.get("jogador2");
-const j3 = dadosForm.get("jogador3");
-const j4 = dadosForm.get("jogador4");
-const j5 = dadosForm.get("jogador5");
+    // 2. Seleciona o formulário e adiciona o evento de "submit"
+    const formTime = document.getElementById("form-time");
 
-// 4. Cria um objeto TimeFutsal
-const novoTime = new TimeFutsal(nomeTime, j1. j2, j3, j4, j5);
+    // 3. Captura de dados usando FormData
+    const dadosForm = new FormData(formTime);
+    const nomeTime = dadosForm.get("nomeTime");
+    const j1 = dadosForm.get("jogador1");
+    const j2 = dadosForm.get("jogador2");
+    const j3 = dadosForm.get("jogador3");
+    const j4 = dadosForm.get("jogador4");
+    const j5 = dadosForm.get("jogador5");
 
-//5. Puxa lista de times do LocalStorage
-const listaTimesJSON = localStorage.getItem("timesInscritos");
-let listaTimes = [];
+    // 4. Cria um objeto TimeFutsal
+    const novoTime = new TimeFutsal(nomeTime, j1. j2, j3, j4, j5);
 
-if(listaTimesJSON){
-    listaTimes = JSON.parse(listaTimesJSON);
-}
+    //5. Puxa lista de times do LocalStorage
+    const listaTimesJSON = localStorage.getItem("timesInscritos");
+    let listaTimes = [];
 
-// 6. Adiciona novoTime à lista e salva de volta no LocalStorage
-listaTimes.push(nomeTime);
-localStorage.setItem("timesInscritos", JSON.stringify(listaTimes));
+    if(listaTimesJSON){
+        listaTimes = JSON.parse(listaTimesJSON);
+    }
 
-// 7. Confirmação para o usuário
-alert("Time inscrito com sucesso: \n" + novoTime.descricao());
+    // 6. Adiciona novoTime à lista e salva de volta no LocalStorage
+    listaTimes.push(nomeTime);
+    localStorage.setItem("timesInscritos", JSON.stringify(listaTimes));
 
-//8. Limpa formulário
-formTime.reset();
+    // 7. Confirmação para o usuário
+    alert("Time inscrito com sucesso: \n" + novoTime.descricao());
+
+    //8. Limpa formulário
+    formTime.reset();
+    
+});
